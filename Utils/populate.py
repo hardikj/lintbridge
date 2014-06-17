4dace6639d430c75d26d7efb4ed5184116c092fe
Hardik Juneja <hardikjuneja.hj@gmail.com>
2014-06-17 11:39:09 +0530

    updated python script

--- Utils/populate.py
+++ Utils/populate.py
@@ -1,17 +1,32 @@
 from bs4 import BeautifulSoup
 import urllib3
-import time
+import os, sys
 
 http = urllib3.PoolManager()
 
-r = http.request('GET', 'parsoid-tests.wikimedia.org/topfails/0')
+s = os.fork()
+if(s!=0):
+    sys.exit()
+pid = os.getpid()
+pidfile = open('populate.pid', 'w')
+pidfile.write(str(pid))
+pidfile.close()
 
-soup = BeautifulSoup (r.data)
+logfile = open('session.log', 'w')
 
-for tr in soup.find_all(status='fail'):
-	a = tr.td.find_all('a')
-	fullLink = a[1].get('href')
-	href = ''.join(fullLink.split('/_rt'))
-	r = http.request('GET', href)
-	time.sleep(3)
-	print("done")
+
+for i in range(1,50):
+	r = http.request('GET', 'parsoid-tests.wikimedia.org/topfails/%s'%i)
+	print("fetched page -%s"%i)
+	logfile.write("fetched page -%s"%i)
+	soup = BeautifulSoup (r.data)
+
+	for tr in soup.find_all('tr', {'status': ['skip', 'fail']}):
+		a = tr.td.find_all('a')
+		fullLink = a[1].get('href')
+		href = ''.join(fullLink.split('/_rt'))
+		r = http.request('GET', href)
+		print("done")
+		logfile.write("done -%s"%i)
+
+logfile.close()
